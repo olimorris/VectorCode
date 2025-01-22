a117ee83482d5b3ab55183ef2fc81b9a3df8ef5d
Davidyz <hzjlyz@gmail.com>
2025-01-22 10:11:57 +0000

    fix(cli): change the output message when running `init`

--- src/vectorcode/subcommands/init.py
+++ src/vectorcode/subcommands/init.py
@@ -14,14 +14,14 @@ async def init(configs: Config) -> int:
         )
         return 1
     os.makedirs(project_config_dir)
+    print(f"VectorCode project root has been initialised at {configs.project_root}")
     if os.path.isfile(GLOBAL_CONFIG_PATH):
         shutil.copyfile(
             GLOBAL_CONFIG_PATH, os.path.join(project_config_dir, "config.json")
         )
-    print(f"VectorCode project root has been initialised at {configs.project_root}")
-    print(
-        "The global configuration at ~/.config/vectorcode/config.json has been copied to the project config directory."
-    )
+        print(
+            "The global configuration at ~/.config/vectorcode/config.json has been copied to the project config directory."
+        )
     print(
         "Note: The collection in the database will not be created until you vectorise a file."
     )
