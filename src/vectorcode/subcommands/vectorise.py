d2398b2a09bac9355898d2b3cdeb964fba77fcad
Davidyz <hzjlyz@gmail.com>
2025-02-01 16:45:55 +0000

    refactor(cli): move `chunked_add` to an independent function.

--- src/vectorcode/subcommands/vectorise.py
+++ src/vectorcode/subcommands/vectorise.py
@@ -9,6 +9,7 @@ from asyncio import Lock
 import pathspec
 import tabulate
 import tqdm
+from chromadb.api.models.AsyncCollection import AsyncCollection
 from chromadb.api.types import IncludeEnum
 
 from vectorcode.chunking import FileChunker
@@ -25,6 +26,45 @@ def get_uuid() -> str:
     return uuid.uuid4().hex
 
 
+async def chunked_add(
+    file_path: str,
+    collection: AsyncCollection,
+    collection_lock: Lock,
+    stats: dict[str, int],
+    stats_lock: Lock,
+    configs: Config,
+    max_batch_size: int,
+):
+    full_path_str = str(expand_path(str(file_path), True))
+    async with collection_lock:
+        num_existing_chunks = len(
+            (
+                await collection.get(
+                    where={"path": full_path_str},
+                    include=[IncludeEnum.metadatas],
+                )
+            )["ids"]
+        )
+    if num_existing_chunks:
+        async with collection_lock:
+            await collection.delete(where={"path": full_path_str})
+        async with stats_lock:
+            stats["update"] += 1
+    else:
+        async with stats_lock:
+            stats["add"] += 1
+    with open(full_path_str) as fin:
+        chunks = list(FileChunker(configs.chunk_size, configs.overlap_ratio).chunk(fin))
+        async with collection_lock:
+            for idx in range(0, len(chunks), max_batch_size):
+                inserted_chunks = chunks[idx : idx + max_batch_size]
+                await collection.add(
+                    ids=[get_uuid() for _ in inserted_chunks],
+                    documents=inserted_chunks,
+                    metadatas=[{"path": full_path_str} for _ in inserted_chunks],
+                )
+
+
 async def vectorise(configs: Config) -> int:
     client = await get_client(configs)
     try:
@@ -34,12 +74,16 @@ async def vectorise(configs: Config) -> int:
         return 1
     if not verify_ef(collection, configs):
         return 1
-    files = await expand_globs(configs.files or [], recursive=configs.recursive)
-
     gitignore_path = os.path.join(str(configs.project_root), ".gitignore")
+    files = await expand_globs(configs.files or [], recursive=configs.recursive)
     if os.path.isfile(gitignore_path):
         with open(gitignore_path) as fin:
             gitignore_spec = pathspec.GitIgnoreSpec.from_lines(fin.readlines())
+        files = [
+            file
+            for file in files
+            if (configs.force or not gitignore_spec.match_file(file))
+        ]
     else:
         gitignore_spec = None
 
@@ -48,51 +92,24 @@ async def vectorise(configs: Config) -> int:
     stats_lock = Lock()
     max_batch_size = await client.get_max_batch_size()
 
-    async def chunked_add(file_path: str):
-        if (
-            (not configs.force)
-            and gitignore_spec is not None
-            and gitignore_spec.match_file(file_path)
-        ):
-            # handles gitignore.
-            return
-
-        full_path_str = str(expand_path(str(file_path), True))
-        async with collection_lock:
-            num_existing_chunks = len(
-                (
-                    await collection.get(
-                        where={"path": full_path_str},
-                        include=[IncludeEnum.metadatas],
-                    )
-                )["ids"]
-            )
-        if num_existing_chunks:
-            async with collection_lock:
-                await collection.delete(where={"path": full_path_str})
-            async with stats_lock:
-                stats["update"] += 1
-        else:
-            async with stats_lock:
-                stats["add"] += 1
-        with open(full_path_str) as fin:
-            chunks = list(
-                FileChunker(configs.chunk_size, configs.overlap_ratio).chunk(fin)
-            )
-            async with collection_lock:
-                for idx in range(0, len(chunks), max_batch_size):
-                    inserted_chunks = chunks[idx : idx + max_batch_size]
-                    await collection.add(
-                        ids=[get_uuid() for _ in inserted_chunks],
-                        documents=inserted_chunks,
-                        metadatas=[{"path": full_path_str} for _ in inserted_chunks],
-                    )
-
     with tqdm.tqdm(
         total=len(files), desc="Vectorising files...", disable=configs.pipe
     ) as bar:
         try:
-            tasks = [asyncio.create_task(chunked_add(str(file))) for file in files]
+            tasks = [
+                asyncio.create_task(
+                    chunked_add(
+                        str(file),
+                        collection,
+                        collection_lock,
+                        stats,
+                        stats_lock,
+                        configs,
+                        max_batch_size,
+                    )
+                )
+                for file in files
+            ]
             for task in asyncio.as_completed(tasks):
                 await task
                 bar.update(1)
