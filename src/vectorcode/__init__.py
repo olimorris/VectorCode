18accbf3e42932bbb285b1028430608c2ff0b4e0
Davidyz <hzjlyz@gmail.com>
2025-02-20 10:43:38 +0000

    chore(cli): use `scm` version scheme.

--- src/vectorcode/__init__.py
+++ src/vectorcode/__init__.py
@@ -1,1 +1,5 @@
-__version__ = "0.3.3"
+try:
+    # this will be populated by pdm build backend when building.
+    from vectorcode._version import __version__
+except Exception:
+    __version__ = "0.0.0"
