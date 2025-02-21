bf34d01ad9c6d081b0aa5c2292bec18baa87cfca
Davidyz <hzjlyz@gmail.com>
2025-02-21 22:09:18 +0000

    fix(nvim): updated system prompts.

--- lua/vectorcode/integrations/codecompanion.lua
+++ lua/vectorcode/integrations/codecompanion.lua
@@ -57,10 +57,10 @@ local make_tool = check_cli_wrap(function(opts)
   - **Don't escape** special characters
   - Make sure the tools xml block is **surrounded by ```xml**
   - seperate phrases into distinct keywords when appropriate
-  - For a symbol defined/declared in a different file, this tool may be able to find the definition. Add the name of the symbol to the query keywords if needed
-  - The embeddings are generated from source code, so using keywords that may be used as a variable name or are valid statements in the programming language may help with the retrieval
+  - If a class, type or function has been imported from another file, this tool may be able to find its source. Add the name of the imported symbol to the query
+  - The embeddings are mostly generated from source code, so using keywords that may be present in source code may help with the retrieval
   - The path of a retrieved file will be wrapped in `<path>` and `</path>` tags. Its content will be right after the `</path>` tag, wrapped by `<content>` and `</content>` tags
-  - If you used the tool, tell users that they may need to wait for the results and there will be a virtual text indicator that shows the tool is still running
+  - If you used the tool, tell users that they may need to wait for the results and there will be a virtual text indicator showing the tool is still running
   - Avoid retrieving one single file because the retrieval mechanism may not be very accurate
   - When providing answers based on VectorCode results, try to give references such as paths to files and line ranges, unless you're told otherwise
   %s
